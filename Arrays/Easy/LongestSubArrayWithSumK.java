2bd64761de25dbc80f326910a54502231aa52fe9
Rohit <rohitghate78692@gmail.com>
2024-02-18 17:29:39 +0530

    Arrays Easy Questions

---
+++ Arrays/Easy/LongestSubArrayWithSumK.java
@@ -0,0 +1,13 @@
+public class LongestSubArrayWithSumK {
+    
+    public static int longestSubarrayWithSumK(int []a, long k) {
+        
+    } 
+    
+    public static void main(String[] args) {
+        int n = 7;
+        int k = 3;
+        int arr [] = {1, 2, 3, 1, 1, 1, 1};
+        System.out.println(longestSubarrayWithSumK(arr, k));
+    }
+}
